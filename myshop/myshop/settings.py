bf1daf9d35fca2a2acfedd01d8fd755475d56827
jarpit646@gmail.com <jarpit646@gmail.com>
2023-02-23 19:20:52 +0530

    Sessions at CART

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart',
 
 ]
 
@@ -128,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+CART_SESSION_ID = 'cart'
